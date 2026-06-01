8f0f0eddb397afae89d69e371d4cb97bae065887
2025020071 <sonjiho2006@gmail.com>
2026-06-01 15:04:19 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    char arr[10];
+    char *target = nullptr;
+    char *find = nullptr;
+
+    for (target = arr; target < arr + 10; target++)
+        scanf("%c", target);
+        getchar();
+
+    char maxChar  = arr[0];
+    int  maxCount = 0;
+
+    for (target = arr; target < arr + 10; target++) {
+        int cnt = 0;
+
+        for (find = arr; find < arr + 10; find++) {
+            if (*target == *find)
+                cnt++;
+        }
+
+        if (cnt > maxCount) {
+            maxCount = cnt;
+            maxChar  = *target;
+        }
+    }
+
+    printf("%c %d\n", maxChar, maxCount);
+    return 0;
+}
